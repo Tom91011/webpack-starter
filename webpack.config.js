0eff81fad8776b8af4052441cad19ca2287a8a8a
Tom91011 <tomnightingale@hotmail.com>
2021-10-19 10:14:00 +0100

    2nd Commit - HtmlWebPackPlugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
 
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+   },
+  plugins: [
+   new HtmlWebpackPlugin({
+     title: 'Webpack starter',
+   }),
+  ],
   module: {
     rules: [
       {
