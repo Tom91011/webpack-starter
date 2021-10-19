d099cb4cc4d729de92179b35ad7788b29c67a609
Tom91011 <tomnightingale@hotmail.com>
2021-10-19 13:00:29 +0100

    Add SASS and the ability to add images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+var _ = require('lodash');
+
 import { wizard } from "./wizard"
 import { barbarian } from "./barbarian"
+import "./style.scss";
+import printMe from './print.js';
 
 console.log("Ran from index.js");
 console.log(wizard);
@@ -8,3 +12,19 @@ console.log(barbarian);
 const obj = { a: "alpha", b: "bravo"}
 const newObj = { ...obj, c: "charlie"}
 console.log(newObj);
+
+function component() {
+    const element = document.createElement('div');
+   const btn = document.createElement('button');
+
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+   btn.innerHTML = 'Click me and check the console!';
+   btn.onclick = printMe;
+
+   element.appendChild(btn);
+
+   return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,19 @@ module.exports = {
 
   entry: {
     index: './src/index.js',
+    print: './src/print.js',
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
    },
   plugins: [
    new HtmlWebpackPlugin({
+     template: "./src/template.html",
      title: 'Webpack starter',
+     hash: true,
+     filename: './template.html',
    }),
   ],
   module: {
@@ -26,7 +30,32 @@ module.exports = {
           // without additional settings , this will reference babelrc
           loader: "babel-loader"
         }
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      // {
+      //   test: /\.(svg|png|jpg|gif)$/i,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       name: "[name].[hash].[ext]",
+      //       outputPath: "imgs"
+      //     }
+      //   }
+      // }
     ]
   },
 
